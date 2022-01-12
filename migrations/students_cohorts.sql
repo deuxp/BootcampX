9bc5d25768e1f0a277e1369a0beccde3f68477cc
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-01-12 15:07:01 -0500

    init commit

---
+++ migrations/students_cohorts.sql
@@ -0,0 +1,20 @@
+CREATE  table cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  cohort_name VARCHAR(255) NOT NULL,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY NOT NULL,
+  student_name VARCHAR(255),
+  email VARCHAR(255),
+  phone VARCHAR(32),
+  github VARCHAR(255),
+  start_date DATE,
+  end_date DATE,
+  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
+);
+
+ALTER TABLE cohorts 
+RENAME COLUMN cohort_name TO name;
